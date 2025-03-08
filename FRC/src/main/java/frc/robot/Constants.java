008fd1dd0c706ea937b42357c00dfd403ec9a894
Shuwhits <123417941+T3rryYan9@users.noreply.github.com>
2025-03-08 18:10:21 -0500

    Add controller port and sparkflex ID for algaearm

--- FRC/src/main/java/frc/robot/Constants.java
+++ FRC/src/main/java/frc/robot/Constants.java
@@ -14,6 +14,8 @@ package frc.robot;
  */
 public final class Constants {
   public static class OperatorConstants {
+    public static final int kOperatorControllerPort = 1;
     public static final int kDriverControllerPort = 0;
+    public static final int AlgaeArmFlexID = 9;
   }
 }
