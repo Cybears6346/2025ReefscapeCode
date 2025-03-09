0724b36c931c620a05642e7d1eb62c1aeb3b2f20
daryljc <daryl.jimenez.c@gmail.com>
2025-03-09 11:02:06 -0400

    TODO: confirm that 'init' is needed for each comm

--- FRC/src/main/java/frc/robot/Constants.java
+++ FRC/src/main/java/frc/robot/Constants.java
@@ -17,5 +17,7 @@ public final class Constants {
     public static final int kOperatorControllerPort = 1;
     public static final int kDriverControllerPort = 0;
     public static final int AlgaeArmFlexID = 9;
+    public static final int AlgaeWheelMaxID = 10;
   }
+
 }
---
+++ FRC/src/main/java/frc/robot/commands/AlgaeWheelSetSpeed.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+public class AlgaeWheelSetSpeed {
+    // @Override
+    // public void initialize() 
+    // {
+
+    // }
+
+    // @Override
+    // public void execute()
+    // {
+
+    // }
+
+    // @Override
+    // public void end()
+    // {
+
+    // }
+}
