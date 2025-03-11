90c425e201a2d1a433598c6e29f769cc0b700a96
T3rryYan9 <123417941+T3rryYan9@users.noreply.github.com>
2025-03-11 10:01:35 -0400

    Finished with Shooter.java and Elevator. java with proper spark configurations

--- FRC/src/main/java/frc/robot/subsystems/Elevator.java
+++ FRC/src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,16 +1,17 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import frc.robot.Constants;
-import frc.robot.Constants.OperatorConstants;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
-public class Elevator {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.config.SparkMaxConfig;
+import frc.robot.Constants.OperatorConstants;
+
+public class Elevator extends SubsystemBase {
     public final SparkMax elevatorMotor1;
     public final SparkMax elevatorMotor2;
 
@@ -18,16 +19,19 @@ public class Elevator {
     {
         elevatorMotor1 = new SparkMax(OperatorConstants.elevatorMotor1ID, MotorType.kBrushless);
         elevatorMotor2 = new SparkMax(OperatorConstants.elevatorMotor2ID, MotorType.kBrushless);
-        SparkMaxConfig globalElevatorLeaderConfig = new SparkMaxConfig();
+        SparkMaxConfig globalElevatorConfig = new SparkMaxConfig();
+        SparkMaxConfig elevatorInvertedConfig = new SparkMaxConfig();
 
-        globalElevatorLeaderConfig
+        globalElevatorConfig
             .smartCurrentLimit(50)
-            .idleMode(IdleMode.kBrake)
-            .inverted(true);
+            .idleMode(IdleMode.kBrake); 
+        elevatorInvertedConfig
+            .apply(globalElevatorConfig)
+            .inverted(true)
+            .follow(elevatorMotor1);
 
-        elevatorMotor1.configure(globalElevatorLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        elevatorMotor2.configure(globalElevatorLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        
+        elevatorMotor1.configure(globalElevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        elevatorMotor2.configure(elevatorInvertedConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     }
     public void setSpeed(double speed)
---
+++ FRC/src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import frc.robot.Constants;
+import frc.robot.Constants.OperatorConstants;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    public final SparkMax shooterMotor1;
+    public final SparkMax shooterMotor2;
+
+    public Shooter()
+    {
+        shooterMotor1 = new SparkMax(OperatorConstants.shooterMotor1ID, MotorType.kBrushless);
+        shooterMotor2 = new SparkMax(OperatorConstants.shooterMotor2ID, MotorType.kBrushless);
+        SparkMaxConfig globalShooterConfig = new SparkMaxConfig();
+        SparkMaxConfig shooterInvertedConfig = new SparkMaxConfig();
+
+        globalShooterConfig
+            .smartCurrentLimit(50)
+            .idleMode(IdleMode.kBrake);
+        shooterInvertedConfig
+            .apply(globalShooterConfig)
+            .inverted(true)
+            .follow(shooterMotor1);
+
+        shooterMotor1.configure(globalShooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        shooterMotor2.configure(shooterInvertedConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    }
+    public void setSpeed(double speed)
+    {
+        //Inverse speed to adjust for joystick input.
+        shooterMotor1.set(-speed);
+        shooterMotor2.set(-speed);
+    }
+}
