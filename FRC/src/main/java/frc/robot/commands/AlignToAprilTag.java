4e62a0245b1f6fc028a012e70ea02eed08828df6
T3rryYan9 <123417941+T3rryYan9@users.noreply.github.com>
2025-03-18 08:55:02 -0400

    Added sample code to implement tracking on limelight

--- FRC/src/main/java/frc/robot/commands/AlignToAprilTag.java
+++ FRC/src/main/java/frc/robot/commands/AlignToAprilTag.java
@@ -1,10 +1,36 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Driving;
+import frc.robot.subsystems.Limelight;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class AlignToAprilTag {
-    private final PIDController pidController = new PIDController(0.02, 0.001, 0.002);
+public class AlignToAprilTag extends Command{
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final Driving driving;
+    private final Limelight limelight;
+    private final PIDController pidController;
+    private boolean isFinished = false;
+    
+    public AlignToAprilTag(Driving driving, Limelight limelight){
+        this.driving = driving;
+        this.limelight = limelight;
+        this.pidController = new PIDController(0.02, 0.001, 0.002);
+        addRequirements(driving);
 
+    }
+    @Override
+    public void execute() {
+        double tx = limelight.getTx(); // Get horizontal offset
+        double turnSpeed = pidController.calculate(tx, 0); // Turn until tx = 0
+        driving.arcadeDrive(0, turnSpeed);
+    }
+    @Override
+    public void end(boolean interrupted){
+        
+    }
+    @Override
+    public boolean isFinished(){
+        return pidController.atSetpoint();
+    }
 }
--- FRC/src/main/java/frc/robot/subsystems/Limelight.java
+++ FRC/src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,5 +1,59 @@
 package frc.robot.subsystems;
 
-public class Limelight {
-    
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Limelight extends SubsystemBase {
+    private final NetworkTable limelightTable;
+
+    public Limelight() {
+        limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    // Get horizontal offset (tx) in degrees
+    public double getTx() {
+        return limelightTable.getEntry("tx").getDouble(0.0);
+    }
+
+    // Get vertical offset (ty) in degrees
+    public double getTy() {
+        return limelightTable.getEntry("ty").getDouble(0.0);
+    }
+
+    // Get target area (ta) as a percentage of image
+    public double getTa() {
+        return limelightTable.getEntry("ta").getDouble(0.0);
+    }
+
+    // Get whether the Limelight has a valid target (tv)
+    public boolean hasTarget() {
+        return limelightTable.getEntry("tv").getDouble(0.0) == 1.0;
+    }
+
+    // Set Limelight LED mode (0 = pipeline, 1 = force off, 2 = force blink, 3 = force on)
+    public void setLedMode(int mode) {
+        limelightTable.getEntry("ledMode").setNumber(mode);
+    }
+
+    // Set Limelight camera mode (0 = vision processing, 1 = driver camera)
+    public void setCameraMode(int mode) {
+        limelightTable.getEntry("camMode").setNumber(mode);
+    }
+
+    // Set Limelight pipeline (0-9)
+    public void setPipeline(int pipeline) {
+        limelightTable.getEntry("pipeline").setNumber(pipeline);
+    }
+
+    @Override
+    public void periodic() {
+        // Display Limelight values on the SmartDashboard for debugging
+        SmartDashboard.putNumber("Limelight TX", getTx());
+        SmartDashboard.putNumber("Limelight TY", getTy());
+        SmartDashboard.putNumber("Limelight TA", getTa());
+        SmartDashboard.putBoolean("Limelight Has Target", hasTarget());
+    }
 }
